8b8e7962f9e92197c5211f7b81f00741c2522b0e
Max mario <63772315+Stormshadows@users.noreply.github.com>
2022-01-12 12:35:54 +0530

    Add files via upload

---
+++ admin/include/footer.php
@@ -0,0 +1,14 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; ShopCart </b>
+            <br> </br>
+            <style>.heart{color:#e25555;}
+                .container{text-align: center}
+            </style>
+            Made with <span class="heart">♥</span> by LM </b>
+
+		</div>
+
+	</div>
